6ed04b6192abcb972c6862476d0e9071a8ac555f
leoamorimr <leoamorimr@gmail.com>
2021-11-10 18:50:36 +0000

    Including POST method

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-
-
 const express = require('express')
 const app = express()
+app.use(express.json())
 
-const courses = [
-  {id:1,name:'course1'},
-  {id:2,name:'course2'},
-  {id:3,name:'course3'},
-  {id:4,name:'course4'}
+const courses = [{
+    id: 1,
+    name: 'course1'
+  },
+  {
+    id: 2,
+    name: 'course2'
+  },
+  {
+    id: 3,
+    name: 'course3'
+  },
+  {
+    id: 4,
+    name: 'course4'
+  }
 ]
 
 app.get('/', (req, res) => {
@@ -21,10 +31,21 @@ app.get('/api/courses', (req, res) => {
 app.get('/api/courses/:id', (req, res) => {
   const course = courses.find(c => c.id === parseInt(req.params.id)) //parseInt is mandatory because id is passed as String
   //Is possible to use var or let instead const
-  res.send(req.params.id)
+  if (!course)
+    res.status(404).send('The course with the given ID was not found')
+  res.send(course)
+})
+
+app.post('/api/courses', (req, res) => {
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name
+  }
+  courses.push(course)
+  res.send(course)
 })
 
 const port = process.env.PORT || 3000
 app.listen(port, () => {
-  console.log(`Listening on port ${port}...`) 
+  console.log(`Listening on port ${port}...`)
 })
