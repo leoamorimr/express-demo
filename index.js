1597252aa7174a1789b8379c518d7dcba5e8c7eb
Leonardo Amorim Ramos <leoamorimr@MBP-de-Leonardo.Home>
2021-11-11 20:40:03 +0000

    Implementation of validation of courses and PUT and POST method

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
+const {
+  response
+} = require('express')
 const express = require('express')
+const Joi = require('joi')
+
 const app = express()
 app.use(express.json())
 
@@ -37,13 +42,49 @@ app.get('/api/courses/:id', (req, res) => {
 })
 
 app.post('/api/courses', (req, res) => {
+  const {
+    error
+  } = validateCourse(req.body) //result.error
+
+  if (error) {
+    res.status(400).send(error.details[0].message)
+    return
+  }
+
   const course = {
     id: courses.length + 1,
     name: req.body.name
   }
+
   courses.push(course)
   res.send(course)
 })
+
+app.put('/api/courses/:id', (req, res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id))
+  if (!course) {
+    res.status(404).send('The course with the given ID was not found')
+  }
+
+  const {
+    error
+  } = validateCourse(req.body) //result.error
+
+  if (error) {
+    res.status(400).send(error.details[0].message)
+    return
+  }
+
+  course.name = req.body.name
+  res.send(course)
+})
+
+function validateCourse(course) {
+  const schema = {
+    name: Joi.string().min(3).required()
+  }
+  return Joi.validate(course, schema)
+}
 
 const port = process.env.PORT || 3000
 app.listen(port, () => {
